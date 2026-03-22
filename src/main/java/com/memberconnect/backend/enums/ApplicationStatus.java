4c09f36097b33386a2cb9149b0a6526e123ea30c
Anjula Amarakoon <prasadanjula1@gmail.com>
2026-03-22 22:12:00 +0530

    Revert "Anju"

--- src/main/java/com/memberconnect/backend/enums/ApplicationStatus.java
+++
@@ -1,9 +0,0 @@
-package com.memberconnect.backend.enums;
-
-public enum ApplicationStatus {
-    NEW,
-    SUBMITTED_FOR_APPROVAL,
-    ADDED_TO_BOARD_APPROVAL_LIST,
-    REJECTED,
-    INACTIVE
-}
--- src/main/java/com/memberconnect/backend/enums/Gender.java
+++
@@ -1,6 +0,0 @@
-package com.memberconnect.backend.enums;
-
-public enum Gender {
-    MALE,
-    FEMALE
-}
